0da37a7b0f54f8990fcbde68ac637011a0984625
joycenerd <chinjoyce30@gmail.com>
2018-06-03 11:42:27 +0800

    finish the program

--- quicksort/main.cpp
+++ quicksort/main.cpp
@@ -12,62 +12,72 @@
 #include <vector>
 #define NUM 100
 #define N 300
+#define LEN 20
 using namespace std;
+
+
+int numcmp(void *point1, void *point2){
+    return *((int *)point1)-*((int *)point2);
+}  //sort number的compare function
 
 typedef struct{
     int num1;
     int num2;
-}PAIR;
-
-typedef struct{
-    char char1;
-    char char2;
-}CHAR;
-
-typedef struct{
-    char str;
-    int num;
-}MIX;
-
-int mixcmp(void *point1, void *point2){
-    MIX first=*(MIX *)point1, second=*(MIX *)point2;
-    if(first.str-second.str==0) return first.num-second.num;
-    return first.str-second.str;
-}
-
-int charcmp(void *point1,void *point2){
-    CHAR first=*(CHAR *)point1, second=*(CHAR *)point2;
-    if(first.char1-second.char1==0) return first.char2-second.char2;
-    return first.char1-second.char1;
-}
-
-int numcmp(void *point1, void *point2){
-    return *((int *)point1)-*((int *)point2);
-}
+}PAIR;   //（num,num)
 
 int paircmp(void *point1,void *point2){
     PAIR first=*(PAIR *)point1, second=*(PAIR *)point2;
     if(first.num1-second.num1==0) return first.num2-second.num2;
     return first.num1-second.num1;
-}
+} //sort pair的compare function
 
+typedef struct{
+    char char1;
+    char char2;
+}CHAR;   //（char,char)
+
+int charcmp(void *point1,void *point2){
+    CHAR first=*(CHAR *)point1, second=*(CHAR *)point2;
+    if(first.char1-second.char1==0) return first.char2-second.char2;
+    return first.char1-second.char1;
+}   //sort char char的compare function
+
+typedef struct{
+    char str;
+    int num;
+}MIX;  //(char,num)
+
+int mixcmp(void *point1, void *point2){
+    MIX first=*(MIX *)point1, second=*(MIX *)point2;
+    if(first.str-second.str==0) return first.num-second.num;
+    return first.str-second.str;
+}  //sort char num的compare function
+
+//做位置交換的function
 void swap(char *point1,char *point2,size_t size){
     if(point1==point2) return;
-    char *tempMem=(char*)malloc(size);
+    char *tempMem=(char*)malloc(size);  //交換時的暫存空間
     memcpy(tempMem,point1,size);
     memcpy(point1,point2,size);
     memcpy(point2,tempMem,size);
     return;
 }
 
+//做quicksort的function
+// *start:sequence起始位置, *number:整個sequence的個數, *size:船上來的型態單個的size
+//(*cmp): 不同型態搭配的compare function
 void quicksort(void *start, long long int number, int size, int (*cmp) (void*,void*)){
+    //partition到個數剩1的時候結束
     if(number<=1) return;
+    // *pivot:基準值, *base:sequence起始位置
     char *pivot,*base=(char*)start;
-    pivot=base+(number-1)*size;
+    pivot=base+(number-1)*size;  //初始pivot是最後一個元素
+    // *randomPivot: 用random選出來的新的pivot
     char *randomPivot=base+(rand()%number)*size;
     swap(pivot,randomPivot,size);
     char *ptr,*i;
     ptr=base;
+    //做pivot和其他元素的比較，藉此確認pivot應該在的位置
     for(i=base;i<pivot;i+=size){
         if(cmp(i,pivot)<=0){
             swap(i,ptr,size);
@@ -75,6 +85,8 @@ void quicksort(void *start, long long int number, int size, int (*cmp) (void*,vo
         }
     }
     swap(pivot,ptr,size);
+    //splitPoint:pivot前一個元素
+    //因為pivot已經確定，所以就分別繼續quicksort pivot前和pivot後的元素
     long long int splitPoint=(ptr-base)/size;
     quicksort(base,splitPoint,size,cmp);
     quicksort(ptr+size,number-splitPoint-1,size,cmp);
@@ -85,98 +97,114 @@ void quicksort(void *start, long long int number, int size, int (*cmp) (void*,vo
 int main()
 {
     FILE *inputFile,*outputFile;
-    int sequence[NUM];
-    char option[N],inputString[N],input[N];
-    char *token;
-    inputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/inputFile","w");
-    assert(inputFile!=NULL);
-    printf("please enter the type of input: ");
-    fgets(inputString,N,stdin);
-    fprintf(inputFile,"%s",inputString);
-    fclose(inputFile);
-    inputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/inputFile","a+");
-    assert(inputFile!=NULL);
-    fgets(inputString,N,stdin);
-    fprintf(inputFile,"%s",inputString);
-    fclose(inputFile);
-    inputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/inputFile","r");
-    char line[N];
-    int i=0;
-    while(fgets(line,sizeof(line),inputFile)){
-        if(i==0) strncpy(option,line,strlen(line)-1);
-        //printf("%s",option);
-        else strcpy(input,line);
-        i++;
-    }
-    int count=0;
-    char choice[4][20]={"number","pair","char char","char num"};
-    if(strncmp(option,choice[0],6)==0){
-        token = strtok(input, " \0"); //告訴strtok()在哪些地方切開
-        while (token != NULL){
-            sequence[count] = atoi(token); //將字元轉數字
-            count++;
-            token = strtok(NULL, " \0");
+    int i;
+    while(1){
+        //開始將輸入寫入inputFile中
+        inputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/inputFile","w");
+        assert(inputFile!=NULL);
+        //指定要輸入的type：number,(num,num),(char,char),(char,num)
+        printf("please enter the type of input: ");
+        char inputString[N]; //輸入的字串
+        fgets(inputString,N,stdin);
+        fprintf(inputFile,"%s",inputString);
+        fclose(inputFile);
+        //輸入完type之後就輸入要sort的sequence
+        inputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/inputFile","a+");
+        assert(inputFile!=NULL);
+        fgets(inputString,N,stdin);
+        fprintf(inputFile,"%s",inputString);
+        fclose(inputFile);
+        inputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/inputFile","r");
+        //一行一行地將剛才的輸入從inputFile中讀出來處理
+        //line[]是我從inputFile中讀出來的
+        //option[]是存我的input type字串
+        //input[]是要sorting的序列
+        char line[N],option[LEN],input[N];
+        i=0;
+        while(fgets(line,sizeof(line),inputFile)){
+            if(i==0) strncpy(option,line,strlen(line)-1);
+            //printf("%s",option);
+            else strcpy(input,line);
+            i++;
         }
-        for(i=0;i<count;i++) printf("%d ",sequence[i]);
-        printf("\n");
-        quicksort(sequence,count,sizeof(int),numcmp);
-        outputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/outputFile","w");
-        assert(outputFile!=NULL);
-        for(i=0;i<count;i++) fprintf(outputFile,"%d ",sequence[i]);
-        fclose(outputFile);
-            /*for(i=0;i<count;i++){
-            if(i==0) printf("%d",sequence[i]);
-            else printf(" %d",sequence[i]);
+        int count;
+        //choice是所有可輸入的input type
+        char choice[][LEN]={"number","pair","char char","char num","no"};
+        char *token;
+        //終止條件是當option是 no的時候
+        if(strncmp(option,choice[4],2)==0){
+            break;
         }
-        printf("\n");*/
-    }
-    PAIR pair[NUM];
-    if(strncmp(option,choice[1],4)==0){
-        token = strtok(input, " \0"); //告訴strtok()在哪些地方切開
-        while (token != NULL){
-            sscanf(token,"(%d,%d)",&pair[count].num1,&pair[count].num2);
-            count++;
-            token = strtok(NULL, " \0");
+        //sort number情況
+        int sequence[NUM];  //number序列
+        if(strncmp(option,choice[0],6)==0){
+            count=0;
+            token = strtok(input, " \0");
+            while (token != NULL){
+                sequence[count] = atoi(token);
+                count++;
+                token = strtok(NULL, " \0");
+            }
+            //for(i=0;i<count;i++) printf("%d ",sequence[i]);
+            //printf("\n");
+            quicksort(sequence,count,sizeof(int),numcmp);
+            outputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/outputFile","w");
+            assert(outputFile!=NULL);
+            for(i=0;i<count;i++) fprintf(outputFile,"%d ",sequence[i]);
+            fclose(outputFile);
         }
-        for(i=0;i<count;i++) printf("(%d %d) ",pair[i].num1,pair[i].num2);
-        quicksort(pair, count, sizeof(PAIR), paircmp);
-        outputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/outputFile","w");
-        assert(outputFile!=NULL);
-        for(i=0;i<count;i++) fprintf(outputFile,"(%d,%d) ",pair[i].num1,pair[i].num2);
-        fclose(outputFile);
-    }
-    CHAR character[NUM];
-    if(strncmp(option,choice[2],9)==0){
-        token = strtok(input, " \0"); //告訴strtok()在哪些地方切開
-        while (token != NULL){
-            sscanf(token,"(%c,%c)",&character[count].char1,&character[count].char2);
-            count++;
-            token = strtok(NULL, " \0");
+        //sort pair情況
+        PAIR pair[NUM];  //struct (num,num)
+        if(strncmp(option,choice[1],4)==0){
+            count=0;
+            token = strtok(input, " \0"); //告訴strtok()在哪些地方切開
+            while (token != NULL){
+                sscanf(token,"(%d,%d)",&pair[count].num1,&pair[count].num2);
+                count++;
+                token = strtok(NULL, " \0");
+            }
+            for(i=0;i<count;i++) printf("(%d %d) ",pair[i].num1,pair[i].num2);
+            quicksort(pair, count, sizeof(PAIR), paircmp);
+            outputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/outputFile","w");
+            assert(outputFile!=NULL);
+            for(i=0;i<count;i++) fprintf(outputFile,"(%d,%d) ",pair[i].num1,pair[i].num2);
+            fclose(outputFile);
         }
-        for(i=0;i<count;i++) printf("(%c,%c) ",character[i].char1,character[i].char2);
-        quicksort(character, count, sizeof(CHAR), charcmp);
-        outputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/outputFile","w");
-        assert(outputFile!=NULL);
-        for(i=0;i<count;i++)
-            fprintf(outputFile,"(%c,%c) ",character[i].char1,character[i].char2);
-        fclose(outputFile);
-    }
-    MIX mix[NUM];
-    if(strncmp(option,choice[3],8)==0){
-        token = strtok(input, " \0"); //告訴strtok()在哪些地方切開
-        while (token != NULL){
-            sscanf(token,"(%c,%d)",&mix[count].str,&mix[count].num);
-            count++;
-            token = strtok(NULL, " \0");
+        //sort char char pair
+        CHAR character[NUM];  //struct (char,char)
+        if(strncmp(option,choice[2],9)==0){
+            count=0;
+            token = strtok(input, " \0");
+            while (token != NULL){
+                sscanf(token,"(%c,%c)",&character[count].char1,&character[count].char2);
+                count++;
+                token = strtok(NULL, " \0");
+            }
+            for(i=0;i<count;i++) printf("(%c,%c) ",character[i].char1,character[i].char2);
+            quicksort(character, count, sizeof(CHAR), charcmp);
+            outputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/outputFile","w");
+            assert(outputFile!=NULL);
+            for(i=0;i<count;i++)
+                fprintf(outputFile,"(%c,%c) ",character[i].char1,character[i].char2);
+            fclose(outputFile);
         }
-        for(i=0;i<count;i++) printf("(%c,%d) ",mix[i].str,mix[i].num);
-        quicksort(mix, count, sizeof(MIX), mixcmp);
-        outputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/outputFile","w");
-        assert(outputFile!=NULL);
-        for(i=0;i<count;i++)
-            fprintf(outputFile,"(%c,%d) ",mix[i].str,mix[i].num);
-        fclose(outputFile);
+        //sort char num情況
+        MIX mix[NUM];
+        if(strncmp(option,choice[3],8)==0){
+            count=0;
+            token = strtok(input, " \0");
+            while (token != NULL){
+                sscanf(token,"(%c,%d)",&mix[count].str,&mix[count].num);
+                count++;
+                token = strtok(NULL, " \0");
+            }
+            for(i=0;i<count;i++) printf("(%c,%d) ",mix[i].str,mix[i].num);
+            quicksort(mix, count, sizeof(MIX), mixcmp);
+            outputFile=fopen("/Users/joycechin/Desktop/EX5/quicksort/outputFile","w");
+            assert(outputFile!=NULL);
+            for(i=0;i<count;i++) fprintf(outputFile,"(%c,%d) ",mix[i].str,mix[i].num);
+            fclose(outputFile);
+        }
     }
-    
     return 0;
 }
